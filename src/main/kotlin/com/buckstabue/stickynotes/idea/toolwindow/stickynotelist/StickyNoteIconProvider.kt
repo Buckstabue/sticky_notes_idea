d699b88ac995f2abd48d06017ea3df8f1786f953
Vyacheslav Kiyako <buckstabue@inbox.ru>
2019-12-30 03:42:49 +0300

    set new icons for sticky note list

---
+++ src/main/kotlin/com/buckstabue/stickynotes/idea/toolwindow/stickynotelist/StickyNoteIconProvider.kt
@@ -0,0 +1,33 @@
+package com.buckstabue.stickynotes.idea.toolwindow.stickynotelist
+
+import com.buckstabue.stickynotes.FileBoundStickyNote
+import com.buckstabue.stickynotes.NonBoundStickyNote
+import com.buckstabue.stickynotes.StickyNote
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.vfs.VirtualFileManager
+import javax.inject.Inject
+import javax.inject.Singleton
+import javax.swing.Icon
+
+@Singleton
+class StickyNoteIconProvider @Inject constructor() {
+    companion object {
+        private val NON_BOUND_ICON by lazy { IconLoader.getIcon("/non_bound_sticky_note.svg") }
+        private val DEFAULT_BOUND_ICON by lazy { AllIcons.FileTypes.Unknown }
+    }
+
+    fun getIcon(stickyNote: StickyNote): Icon? {
+        return when (stickyNote) {
+            is NonBoundStickyNote -> NON_BOUND_ICON
+            is FileBoundStickyNote -> getIconFromFileBoundStickyNote(stickyNote)
+        }
+    }
+
+    private fun getIconFromFileBoundStickyNote(stickyNote: FileBoundStickyNote): Icon {
+        val virtualFile = VirtualFileManager.getInstance()
+            .findFileByUrl(stickyNote.fileLocation.fileUrl) ?: return DEFAULT_BOUND_ICON
+        return FileTypeManager.getInstance().getFileTypeByFile(virtualFile).icon ?: DEFAULT_BOUND_ICON
+    }
+}
--- src/main/kotlin/com/buckstabue/stickynotes/idea/toolwindow/stickynotelist/StickyNoteListPresenter.kt
+++ src/main/kotlin/com/buckstabue/stickynotes/idea/toolwindow/stickynotelist/StickyNoteListPresenter.kt
@@ -7,18 +7,17 @@ import com.buckstabue.stickynotes.StickyNoteInteractor
 import com.buckstabue.stickynotes.base.BasePresenter
 import com.buckstabue.stickynotes.idea.toolwindow.PerToolWindow
 import com.buckstabue.stickynotes.idea.toolwindow.StickyNoteRouter
-import com.intellij.icons.AllIcons
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.map
 import javax.inject.Inject
-import javax.swing.Icon
 
 @PerToolWindow
 class StickyNoteListPresenter @Inject constructor(
     private val router: StickyNoteRouter,
-    private val stickyNoteInteractor: StickyNoteInteractor
+    private val stickyNoteInteractor: StickyNoteInteractor,
+    private val stickyNoteIconProvider: StickyNoteIconProvider
 ) : BasePresenter<StickyNoteListView>() {
 
     private var viewModel = StickyNoteListViewModel(
@@ -55,16 +54,9 @@ class StickyNoteListPresenter @Inject constructor(
         return stickyNotes.map {
             StickyNoteViewModel(
                 description = it.description,
-                icon = getIcon(it),
+                icon = stickyNoteIconProvider.getIcon(it),
                 stickyNote = it
             )
-        }
-    }
-
-    private fun getIcon(stickyNote: StickyNote): Icon? {
-        return when (stickyNote) {
-            is NonBoundStickyNote -> AllIcons.General.Note
-            is FileBoundStickyNote -> AllIcons.Actions.StepOutCodeBlock
         }
     }
 
