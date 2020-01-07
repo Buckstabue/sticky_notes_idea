6aadd5688ef45d41ee2b68f18426dd2f3a2b3f3c
Vyacheslav Kiyako <buckstabue@inbox.ru>
2020-01-07 19:44:16 +0300

    add a TODO for custom MainScope class

--- src/main/kotlin/com/buckstabue/stickynotes/idea/SnMainScope.kt
+++ src/main/kotlin/com/buckstabue/stickynotes/idea/SnMainScope.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlin.coroutines.CoroutineContext
 
+// TODO remove me when min version is updated or bug https://youtrack.jetbrains.com/issue/IDEA-229657 is resolved
 class MainScope : CoroutineScope {
     override val coroutineContext: CoroutineContext = SupervisorJob(parent = null) + Dispatchers.Main
 
