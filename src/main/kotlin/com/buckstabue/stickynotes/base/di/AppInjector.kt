3837e3321828253578967d8fb332b1862333e067
Vyacheslav Kiyako <buckstabue@inbox.ru>
2020-01-31 00:40:12 +0300

    fix memory leak when switching between projects

--- src/main/kotlin/com/buckstabue/stickynotes/base/di/AppInjector.kt
+++ src/main/kotlin/com/buckstabue/stickynotes/base/di/AppInjector.kt
@@ -4,15 +4,18 @@ import com.buckstabue.stickynotes.base.di.app.AppComponent
 import com.buckstabue.stickynotes.base.di.app.DaggerAppComponent
 import com.buckstabue.stickynotes.base.di.project.ProjectComponent
 import com.intellij.openapi.project.Project
-import java.util.concurrent.ConcurrentHashMap
+import java.util.Collections
+import java.util.WeakHashMap
 
 object AppInjector {
-    private val projectComponentMap: MutableMap<Project, ProjectComponent> = ConcurrentHashMap()
+    private val projectComponentMap: MutableMap<Project, ProjectComponent> =
+        Collections.synchronizedMap(WeakHashMap())
 
     val appComponent: AppComponent by lazy { DaggerAppComponent.create() }
 
     fun getProjectComponent(project: Project): ProjectComponent {
-        return projectComponentMap.getOrPut(project, { appComponent.plusProjectComponent().create(project) })
+        return projectComponentMap
+            .getOrPut(project, { appComponent.plusProjectComponent().create(project) })
     }
 
     fun releaseProjectComponent(project: Project) {
