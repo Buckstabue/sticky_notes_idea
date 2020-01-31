d47fd64a12a5aba8e3c55a6a96a6a16c91fa52c5
Vyacheslav Kiyako <buckstabue@inbox.ru>
2020-01-31 04:07:22 +0300

    make IdeaAdvertisementIdProvider a dagger singleton

--- src/main/kotlin/com/buckstabue/stickynotes/base/di/app/AppModule.kt
+++ src/main/kotlin/com/buckstabue/stickynotes/base/di/app/AppModule.kt
@@ -8,21 +8,13 @@ import com.buckstabue.stickynotes.idea.util.IdeaDeviceInfo
 import com.buckstabue.stickynotes.util.DeviceInfo
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 interface AppModule {
 
-    @Module
-    companion object {
-        @Provides
-        @Singleton
-        @JvmStatic
-        fun provideAdvertisementIdProvider(): AdvertisementIdProvider {
-            return IdeaAdvertisementIdProvider.getInstance()
-        }
-    }
+    @Binds
+    fun bindAdvertisementIdProvider(
+        advertisementIdProvider: IdeaAdvertisementIdProvider): AdvertisementIdProvider
 
     @Binds
     fun bindDeviceInfo(deviceInfo: IdeaDeviceInfo): DeviceInfo
--- src/main/kotlin/com/buckstabue/stickynotes/idea/analytics/IdeaAdvertisementIdProvider.kt
+++ src/main/kotlin/com/buckstabue/stickynotes/idea/analytics/IdeaAdvertisementIdProvider.kt
@@ -3,23 +3,19 @@ package com.buckstabue.stickynotes.idea.analytics
 import com.buckstabue.stickynotes.analytics.AdvertisementIdProvider
 import com.buckstabue.stickynotes.idea.IdeaConst
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.openapi.components.ServiceManager
 import java.util.UUID
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class IdeaAdvertisementIdProvider : AdvertisementIdProvider {
+@Singleton
+class IdeaAdvertisementIdProvider @Inject constructor() : AdvertisementIdProvider {
     companion object {
         private const val KEY_ADV_ID = "${IdeaConst.PROPERTY_PREFIX}.adv_id"
-
-        fun getInstance(): AdvertisementIdProvider {
-
-            return ServiceManager.getService(
-                AdvertisementIdProvider::class.java
-            )
-        }
     }
 
+    private val propertiesComponent by lazy { PropertiesComponent.getInstance() }
+
     override fun getOrCreateDeviceId(): String {
-        val propertiesComponent = PropertiesComponent.getInstance()
         val storedId = propertiesComponent.getValue(KEY_ADV_ID)
         if (storedId != null) {
             return storedId
